f2b3850bc633ba6409301c6051624af104dedfff
Krushulang <krushulang@gmail.com>
2023-10-14 12:16:00 -0700

    Updates of html

--- app.py
+++ app.py
@@ -11,20 +11,23 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/atrain', methods=['GET'])
+def atrain():
+   print('training AI')
+   #AI train model work
+   return redirect(url_for('index'))
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/teams', methods=['GET'])
+def teams():
+   team1 = request.form.get('team1')
+   team2 = request.form.get('team2')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+   if team1:
+       print('Request for teams received with team names1=%s' % team1 + ' team2=%s' % team2)
+       #AI model work here
+       return render_template('index.html', team1 = team1, team2 = team2)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
+       print('Request for teams received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
 
